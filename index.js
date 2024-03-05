c77981289982b6891773e739e26bb8dcd0aeccb7
Samuel Castillo <samuelcastillo@MacBook-Pro-de-Samuel.local>
2024-03-04 19:36:46 -0600

    salida a prod

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const player = document.getElementById("video")
-
+const ancho = window.screen.width
+console.log(ancho)
 player.addEventListener("click", ()=>{
     player.innerHTML = ""
-    player.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/kevn0vUFYjU?si=AZmjahXc5LnjBmt7" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay="true"; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
+    
+    player.innerHTML = `<iframe width=${ancho} height="200" src="https://www.youtube.com/embed/kevn0vUFYjU?si=AZmjahXc5LnjBmt7" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay="true"; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
 })
